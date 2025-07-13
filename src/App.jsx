8ad6f934f8445bfffb9e1f6def4b80a4ad0dfc2f
Iamryowibowo <riowibowo0211@gmail.com>
2025-07-13 21:08:30 +0700

    Update section contact dan animasi tombol

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <ShowcaseSection />
-      <Logo />
+      {/* <Logo /> */}
       <FeatureCards />
       <Experience />
       <TechStack />
